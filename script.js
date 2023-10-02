9a5fef6892ebed2f70dde34dac9da893668b26b5
renanzinbsb <146401320+renanzinbsb@users.noreply.github.com>
2023-10-01 22:27:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menuVisible = false;
+//menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+//scroll menu
+window.onscroll = function(){
+    efectoHabilidades();
+} 
